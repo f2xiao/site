e8bf185f4a8140526f56d9ef7c7d595b757dea4f
f2xiao <fxiao.mecheng@gmail.com>
2022-02-26 20:32:00 -0500

    Edit App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> 
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,24 +9,25 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+ @import './assets/styles/new.css';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+ #nav{
+   position: fixed;
+   right: 2rem;
+   top: 50%;
+   transform: translateY(-3.5rem);
+   
+   a{
+     display:block;
+     text-decoration: none;
+     border: 1px solid #ccc;
+     width: 3rem;
+     height: 3rem;
+     border-radius: 50%;
+     text-align: center;
+     line-height: 3rem;
+     font-size:.8rem;
+     margin-top: 1rem;
+   }
+ }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <header><h1>Name</h1></header>
+    <p><strong>Web Developer.</strong>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, deleniti blanditiis, accusamus dicta repellendus aliquam culpa quasi, molestias corporis expedita earum possimus nemo officiis minus molestiae similique excepturi asperiores hic.
+    </p>
+      <h2>Short notes</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla adipisci provident, optio laboriosam, animi, quae repellat alias mollitia repellendus doloremque earum! Repellendus, optio quasi aliquid nobis obcaecati libero praesentium expedita.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla adipisci provident, optio laboriosam, animi, quae repellat alias mollitia repellendus doloremque earum! Repellendus, optio quasi aliquid nobis obcaecati libero praesentium expedita.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla adipisci provident, optio laboriosam, animi, quae repellat alias mollitia repellendus doloremque earum! Repellendus, optio quasi aliquid nobis obcaecati libero praesentium expedita.</p>
+      <h2>Posts</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rerum praesentium laboriosam magni a numquam sapiente pariatur maiores consectetur, eveniet ab aliquam est natus dignissimos tenetur quis ipsam harum illo!</p>
+    <footer>Name</footer>
   </div>
 </template>
-
+<style lang="scss">
+  .home{
+    max-width: 36rem;
+    margin: auto;
+  }
+</style>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
