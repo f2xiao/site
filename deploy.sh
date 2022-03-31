ead1cb6e8d6e7e2a9e8675b3b1dfa2ac5b0bc9cc
f2xiao <fxiao.mecheng@gmail.com>
2022-03-31 19:26:25 -0400

    Add dist in .gitigore

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+cd dist/
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:f2xiao/site.git gh-pages
+git push -f git@github.com:f2xiao/site.git main:gh-pages
 
 cd -
