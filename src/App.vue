acd290a9669b80047322e180523d1783bbf132f6
f2xiao <fxiao.mecheng@gmail.com>
2022-04-10 20:39:06 -0400

    Add theme switch

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,19 @@
 </style>
 
 <script>
+ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+  // if user prefer light mode
+const sheets = document.getElementsByTagName('link');
+console.log(sheets[2].href );
+sheets[2].href = `${process.env.BASE_URL}styles/atom-one-light.min.css`;
+document.body.setAttribute('theme', 'light');
+}
 
+// watch for changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    const sheets = document.getElementsByTagName('link');
+sheets[2].href = `${process.env.BASE_URL}styles/atom-one-${newColorScheme}.min.css`;
+document.body.setAttribute('theme', newColorScheme);
+});
 </script>
