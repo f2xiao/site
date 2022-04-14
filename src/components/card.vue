c7db52da43532754f0da8be933a4665fcbf19818
f2xiao <fxiao.mecheng@gmail.com>
2022-04-14 12:53:56 -0400

    Add images in ThreePillars

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="card">
-       <div>
-          <img src="" alt="" >
+       <div class="img">
+          <img :src="imgUrl" :alt="app.title" >
       </div>
       <div class="text">
-          <h1>{{app.title}}</h1>
+          <h1><a :href="app.link">{{app.title}}</a></h1>
           <p>{{app.des}}</p>
       </div>
   </div>
 </template>
 
 <script>
+
 export default {
     props:['app'],
-    name: 'Card'
+    name: 'Card',
+    computed:{
+        imgUrl(){
+            return require('@/assets/img/'+this.app.img);
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 .card{
     background: white;
+    .img{
+        width:100%;
+        img{
+            display: block;
+            width: 100%; 
+        }
+    }
 }
 </style>
