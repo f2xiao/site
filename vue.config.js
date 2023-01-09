25846bda6894b0d66ba4ff8ed2d556bc3e206540
arashi4ever <fxiao.mecheng@gmail.com>
2023-01-09 15:04:20 -0500

    Edit deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,9 @@ const markdownItHlJs = require('markdown-it-highlightjs')
 const markdownItLA = require('markdown-it-link-attributes')
 
 module.exports = {
-  publicPath: "/site",
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/site/'
+  : '/',
   configureWebpack: {
     module: {
       rules: [
