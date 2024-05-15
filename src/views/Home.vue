7d0fdfeea84c816a8eac60c033c81de2fea32540
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-15 11:23:16 -0400

    update packages and personal introduction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,92 +1,112 @@
 <template>
   <div class="home">
     <div class="Intro">
-   <header><h1>Fangfang</h1></header>
-    <p><strong>JavaScript Fullstack Developer.</strong>
-    Enjoy buidling apps. Write JavaScript, HTML and CSS.
-    <p>Techstack that I work with: React, Vue, NodeJS, ExpressJS.</p>
-    Love the idea of OOP and abstraction of complex concepts.
-    </p>
-    <p>
-       Formerly worked as an mechanical engineer in automotive industry, specialized in CAD modeling and process engineering. Found passion in coding and started the path as a developer.
-    </p>
-    <p>When not coding, I play games (mostly Fortnite), watch animes and spend time with my family as much as possible. Currently I am learning how to sew.</p>
-    <p>Know me more, at <a href="https://github.com/f2xiao">Github</a>, <a href="#">CV</a>, <a href="#">LeetCode</a>, <a href="#">Linkedin</a> </p>
-  </div>
-   <div class="projects">
-       <h2>Projects</h2>
-        <div>
-         <router-link class="left" :to="projects[0].link">{{projects[0].title}}</router-link>
-        <button class="right disabled"> <span >{{projects[0].tags.join()}}</span></button>
-      </div>
-       <div v-for="project in projects.slice(1)" :key="project.title">
-         <a class="left" :href="project.link">{{project.title}}</a>
-        <button class="right disabled"> <span >{{project.tags.join()}}</span></button>
-      </div>
-      
-  </div>
-   <div class="posts" v-bind:style="{'padding':'2.5rem 0'}">
-     <h2> <router-link to="/posts">Posts</router-link></h2>
-     <div v-for="post in posts" :key="post.filename" >
-      <router-link  :to="{ path: '/posts/'+post.filename, params: {fileName: post.filename }}">{{post.filename}}</router-link>
+      <header><h1>Fangfang</h1></header>
+      <p>
+        <strong>Fullstack Software Developer.</strong> Enjoy buidling apps.
+        Write JavaScript/Typescript, HTML and CSS.
+      </p>
+      Techstack that I work with:
+      <ul>
+        <li>Frontend: React, Vue, Sass</li>
+        <li>Backend: NodeJS, ExpressJS</li>
+        <li>Database: MongoDB, MySQL</li>
+      </ul>
+      Love the idea of OOP and abstraction of complex concepts.
+      <p>
+        Formerly worked as an mechanical engineer in automotive industry,
+        specialized in CAD modeling and process engineering. Found passion in
+        coding and started the path as a developer.
+      </p>
+      <p>
+        When not coding, I play games (mostly Fortnite), watch animes and spend
+        time with my family as much as possible.
+      </p>
+      <p>
+        Know me more, at <a href="https://github.com/f2xiao">Github</a>,
+        <a href="#">CV</a>, <a href="#">LeetCode</a>,
+        <a href="https://www.linkedin.com/in/fangfang-xiao">Linkedin</a>
+      </p>
     </div>
-   </div>
+    <div class="projects">
+      <h2>Projects</h2>
+      <div>
+        <router-link class="left" :to="projects[0].link">{{
+          projects[0].title
+        }}</router-link>
+        <button class="right disabled">
+          <span>{{ projects[0].tags.join() }}</span>
+        </button>
+      </div>
+      <div v-for="project in projects.slice(1)" :key="project.title">
+        <a class="left" :href="project.link">{{ project.title }}</a>
+        <button class="right disabled">
+          <span>{{ project.tags.join() }}</span>
+        </button>
+      </div>
+    </div>
+    <div class="posts" v-bind:style="{ padding: '2.5rem 0' }">
+      <h2><router-link to="/posts">Posts</router-link></h2>
+      <div v-for="post in posts" :key="post.filename">
+        <router-link
+          :to="{
+            path: '/posts/' + post.filename,
+            params: { fileName: post.filename },
+          }"
+          >{{ post.filename }}</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
 <script>
-
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-  name: 'Home',
+  name: "Home",
   computed: mapState({
-    posts: state => state.posts.slice(0,3),
-    projects: state => state.projects
-  })
-}
+    posts: (state) => state.posts.slice(0, 3),
+    projects: (state) => state.projects,
+  }),
+};
 </script>
 <style lang="scss">
- @media screen and (max-width: 480px) {
+@media screen and (max-width: 480px) {
   // header{
   //   padding-top: 0.5em;
   // }
- }
-  .projects{
-    
-   div{
+}
+.projects {
+  div {
     display: flex;
     flex-flow: row wrap;
     align-items: center;
-   }
-    .left{
-      width: 30%;
-      margin-right:5%;
-    }
+  }
+  .left {
+    width: 30%;
+    margin-right: 5%;
+  }
 
-    .right{
-      position: absolute;
-      // padding: 0 1%;
-      right: 0;
-    }
+  .right {
+    position: absolute;
+    // padding: 0 1%;
+    right: 0;
+  }
 
   @media screen and (max-width: 480px) {
-  .left {
-    min-width: 100%;
-  }
-  .right{
-    position: inherit;
-    font-size: 0.8em;
-  }
-}
-
-    a, span{
-      // margin: 0.25em 0;
-      vertical-align: middle;
+    .left {
+      min-width: 100%;
+    }
+    .right {
+      position: inherit;
+      font-size: 0.8em;
     }
   }
+
+  a,
+  span {
+    // margin: 0.25em 0;
+    vertical-align: middle;
+  }
+}
 </style>
-
-
-
-
-
