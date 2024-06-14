f33d19cd923de6284eef1c0b07b4489938c450da
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-06-14 11:26:54 -0400

    edit vue.config.js to publish to github.io page

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,54 @@
 <template>
   <div id="app">
     <router-view />
-   <footer v-bind:style="{'text-align':'right'}"> <img width="50px" height="50px" v-bind:style="{'border-radius': '50%'}" src="./assets/icons/cat.svg" alt="My Cat"></footer>
+    <footer v-bind:style="{ 'text-align': 'right' }">
+      <img
+        width="50px"
+        height="50px"
+        v-bind:style="{ 'border-radius': '50%' }"
+        src="./assets/icons/cat.svg"
+        alt="My Cat"
+      />
+    </footer>
   </div>
-  
 </template>
 
 <style lang="scss" scoped>
- #app{
-   position: relative;
-   max-width: 36em;
-   margin: 0 auto;
+#app {
+  position: relative;
+  width: 80vw;
+  margin: 0 auto;
   //  padding-bottom: 2rem;
+  max-width: 36em;
 }
-
 </style>
 
 <script>
-
-
-function setTheme(colorScheme){
-   const sheets = document.getElementsByTagName('link');
-   sheets[0].href = `${process.env.BASE_URL}favicon-${colorScheme}.svg`;
+function setTheme(colorScheme) {
+  const sheets = document.getElementsByTagName("link");
+  sheets[0].href = `${process.env.BASE_URL}favicon-${colorScheme}.svg`;
   sheets[2].href = `${process.env.BASE_URL}styles/atom-one-${colorScheme}.min.css`;
-  document.body.setAttribute('theme', colorScheme);
+  document.body.setAttribute("theme", colorScheme);
 }
- if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches
+) {
   // if user prefer light mode
-  setTheme('light');
+  setTheme("light");
 }
 
 // watch for changes
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
     const newColorScheme = event.matches ? "dark" : "light";
     setTheme(newColorScheme);
-});
-export default{
+  });
+export default {
   created: function () {
     this.$store.commit("viewposts/init", this.$store.state.posts);
-    document.title = 'Fangfang';
+    document.title = "Fangfang";
   },
-  
-
-
-  
-}
+};
 </script>
